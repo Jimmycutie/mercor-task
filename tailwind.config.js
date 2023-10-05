90fa582d1f2a7dd7e75f22062a87bd51d9fbe510
Mihir <82092317+Jimmycutie@users.noreply.github.com>
2023-10-05 18:16:56 +0530

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'laser': "url('./static/vector.png')"
+        'laser': "url('./static/Vector.png')"
       },
       fontFamily: {
         pop: ['var(--font-surt)'],
